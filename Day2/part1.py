8cee2b3c689d5ef30e0e1f8b5efd813ab4f5936a
Andy Lin <andy.lin.2004@outlook.com>
2023-12-02 00:14:38 -0500

    wham bam day 2 donezo

--- Day2/part1.py
+++ Day2/part1.py
@@ -1,6 +1,28 @@
-# f = open("input.txt", 'r')
-f = open("test_input.txt", 'r')
+import re
+
+f = open("input.txt", 'r')
+# f = open("test_input.txt", 'r')
 input = f.read()
 
-for line in input:
-    
+ctr = 1
+dict = {}
+
+total = 0
+
+for line in input.splitlines():
+    line = line.split(": ")[1]
+    items = re.split(", |; ", line)
+
+    for word in items:
+        info = word.split()
+        if info[1] not in dict:
+            dict[info[1]] = 0
+        dict[info[1]] = max(int(info[0]), dict[info[1]])
+
+    if dict["red"] <= 12 and dict["green"] <= 13 and dict["blue"] <= 14:
+        total += ctr
+
+    dict.clear()
+    ctr += 1
+
+print(total)
--- Day2/part2.py
+++ Day2/part2.py
@@ -1,6 +1,27 @@
-# f = open("input.txt", 'r')
-f = open("test_input.txt", 'r')
+import re
+
+f = open("input.txt", 'r')
+# f = open("test_input.txt", 'r')
 input = f.read()
 
-for line in input:
-    
+ctr = 1
+dict = {}
+
+total = 0
+
+for line in input.splitlines():
+    line = line.split(": ")[1]
+    items = re.split(", |; ", line)
+
+    for word in items:
+        info = word.split()
+        if info[1] not in dict:
+            dict[info[1]] = 0
+        dict[info[1]] = max(int(info[0]), dict[info[1]])
+
+    total += dict["red"] * dict["green"] * dict["blue"]
+
+    dict.clear()
+    ctr += 1
+
+print(total)
