3a543d67b048a7b015213836321a01b3224d4e01
Andy Lin <andy.lin.2004@outlook.com>
2023-12-03 00:41:39 -0500

    day 3 done with a tired fried brain

--- Day3/part1.py
+++ Day3/part1.py
@@ -1,6 +1,44 @@
-# f = open("input.txt", 'r')
-f = open("test_input.txt", 'r')
-input = f.read()
+f = open("input.txt", 'r')
+# f = open("test_input.txt", 'r')
+input = f.read().splitlines()
+total = 0
 
-for line in input:
-    
+for row in range(len(input)):
+    adding = False
+    numstr = ""
+    for col in range(len(input[row])):
+        if input[row][col].isnumeric():
+            numstr += input[row][col]
+            if not adding:
+                if row > 0:
+                    if col > 0:
+                        if not input[row - 1][col - 1].isnumeric() and input[row - 1][col - 1] != ".":
+                            adding = True
+                        if not input[row - 1][col].isnumeric() and input[row - 1][col] != ".":
+                            adding = True
+                        if col < len(input[row]) - 1:
+                            if not input[row - 1][col + 1].isnumeric() and input[row - 1][col + 1] != ".":
+                                adding = True
+                if col > 0:
+                    if not input[row][col - 1].isnumeric() and input[row][col - 1] != ".":
+                        adding = True
+                if col < len(input[row]) - 1:
+                    if not input[row][col + 1].isnumeric() and input[row][col + 1] != ".":
+                        adding = True
+                if row < len(input) - 1:
+                    if col > 0:
+                        if not input[row + 1][col - 1].isnumeric() and input[row + 1][col - 1] != ".":
+                            adding = True
+                        if not input[row + 1][col].isnumeric() and input[row + 1][col] != ".":
+                            adding = True
+                        if col < len(input[row]) - 1:
+                            if not input[row + 1][col + 1].isnumeric() and input[row + 1][col + 1] != ".":
+                                adding = True
+            if col+1 >= len(input[row]) or not input[row][col + 1].isnumeric():
+                print(numstr, adding)
+                if adding:
+                    total += int(numstr)
+                numstr = ""
+                adding = False
+                    
+print(total)
--- Day3/part2.py
+++ Day3/part2.py
@@ -1,6 +1,61 @@
-# f = open("input.txt", 'r')
-f = open("test_input.txt", 'r')
-input = f.read()
+f = open("input.txt", 'r')
+# f = open("test_input.txt", 'r')
+input = f.read().splitlines()
 
-for line in input:
-    
+gearratios = {}
+total = 0
+
+for row in range(len(input)):
+    adding = False
+    curgearcoord = None
+    numstr = ""
+    for col in range(len(input[row])):
+        if input[row][col].isnumeric():
+            numstr += input[row][col]
+            if not adding:
+                if row > 0:
+                    if col > 0:
+                        if not input[row - 1][col - 1].isnumeric() and input[row - 1][col - 1] == "*":
+                            adding = True
+                            curgearcoord = (row -1, col - 1)
+                        if not input[row - 1][col].isnumeric() and input[row - 1][col] == "*":
+                            adding = True
+                            curgearcoord = (row - 1, col)
+                        if col < len(input[row]) - 1:
+                            if not input[row - 1][col + 1].isnumeric() and input[row - 1][col + 1] == "*":
+                                adding = True
+                                curgearcoord = (row - 1, col + 1)
+                if col > 0:
+                    if not input[row][col - 1].isnumeric() and input[row][col - 1] == "*":
+                        adding = True
+                        curgearcoord = (row, col -1)
+                if col < len(input[row]) - 1:
+                    if not input[row][col + 1].isnumeric() and input[row][col + 1] == "*":
+                        adding = True
+                        curgearcoord = (row, col+1)
+                if row < len(input) - 1:
+                    if col > 0:
+                        if not input[row + 1][col - 1].isnumeric() and input[row + 1][col - 1] == "*":
+                            adding = True
+                            curgearcoord = (row+1, col-1)
+                        if not input[row + 1][col].isnumeric() and input[row + 1][col] == "*":
+                            adding = True
+                            curgearcoord = (row+1, col)
+                        if col < len(input[row]) - 1:
+                            if not input[row + 1][col + 1].isnumeric() and input[row + 1][col + 1] == "*":
+                                adding = True
+                                curgearcoord = (row+1, col+1)
+            if col+1 >= len(input[row]) or not input[row][col + 1].isnumeric():
+                print(numstr, adding)
+                if adding:
+                    if curgearcoord not in gearratios:
+                        gearratios[curgearcoord] = []
+                    gearratios[curgearcoord].append(int(numstr))
+                numstr = ""
+                adding = False
+                    
+for gear in gearratios:
+    if len(gearratios[gear]) == 2:
+        total += gearratios[gear][0] * gearratios[gear][1]
+
+print(total)
