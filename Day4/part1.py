82f5e485721a9be1ba7c0297d18e5a0b19c09301
Andy Lin <andy.lin.2004@outlook.com>
2023-12-04 00:28:10 -0500

    day 4 done

--- Day4/part1.py
+++ Day4/part1.py
@@ -1,6 +1,26 @@
-# f = open("input.txt", 'r')
-f = open("test_input.txt", 'r')
-input = f.read()
+f = open("input.txt", 'r')
+# f = open("test_input.txt", 'r')
+input = f.read().splitlines()
+
+total = 0
 
 for line in input:
+    lcltotal = 0
+    line = line.split(":")[1]
+    line = line[1:]
     
+    nums = line.split("|")
+
+    winning = [int(x) for x in nums[0].split()]
+    found = [int(x) for x in nums[1].split()]
+
+    for num in found:
+        if num in winning:
+            if lcltotal == 0:
+                lcltotal = 1
+            else:
+                lcltotal *= 2
+
+    total += lcltotal
+
+print(total)
--- Day4/part2.py
+++ Day4/part2.py
@@ -1,6 +1,37 @@
-# f = open("input.txt", 'r')
-f = open("test_input.txt", 'r')
-input = f.read()
+f = open("input.txt", 'r')
+# f = open("test_input.txt", 'r')
+input = f.read().splitlines()
+
+total = 0
+eachwinning = []
+instances = []
 
 for line in input:
+    lcltotal = 0
+    line = line.split(":")[1]
+    line = line[1:]
     
+    nums = line.split("|")
+
+    winning = [int(x) for x in nums[0].split()]
+    found = [int(x) for x in nums[1].split()]
+
+    for num in found:
+        if num in winning:
+            lcltotal += 1
+
+    eachwinning.append(lcltotal)
+    instances.append(1)
+
+# print(eachwinning)
+
+for i in range(len(eachwinning)):
+    # print(i)
+    for v in range(eachwinning[i]):
+        instances[i + v + 1] += 1 * instances[i]
+    # print(instances)
+    
+# print(instances)
+total = sum(instances)
+
+print(total)
