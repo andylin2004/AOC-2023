e023b165e6e5b706f2bf216bedfe0ae219c2b470
Andy Lin <andy.lin.2004@outlook.com>
2023-12-08 00:24:50 -0500

    aoc day 8 done

--- Day8/part1.py
+++ Day8/part1.py
@@ -1,6 +1,26 @@
-# f = open("input.txt", 'r')
-f = open("test_input.txt", 'r')
-input = f.read()
+f = open("input.txt", 'r')
+# f = open("test_input.txt", 'r')
+input = f.read().splitlines()
 
-for line in input:
-    
+mode = input[0]
+
+nodes = {}
+
+for line in input[2:]:
+    line = line.split()
+    node_name = line[0]
+    node_left = line[2][1:-1]
+    node_right = line[3][:-1]
+
+    nodes[node_name] = (node_left, node_right)
+
+curNode = "AAA"
+count = 0
+while curNode != "ZZZ":
+    if mode[count % len(mode)] == "L":
+        curNode = nodes[curNode][0]
+    else:
+        curNode = nodes[curNode][1]
+    count += 1
+
+print(count)
--- Day8/part2.py
+++ Day8/part2.py
@@ -1,6 +1,46 @@
-# f = open("input.txt", 'r')
-f = open("test_input.txt", 'r')
-input = f.read()
+from math import lcm
 
-for line in input:
-    
+f = open("input.txt", 'r')
+# f = open("test_input.txt", 'r')
+input = f.read().splitlines()
+
+mode = input[0]
+
+nodes = {}
+nodes_end_A = {}
+nodes_end_Z = {}
+
+for line in input[2:]:
+    line = line.split()
+    node_name = line[0]
+    node_left = line[2][1:-1]
+    node_right = line[3][:-1]
+
+    nodes[node_name] = (node_left, node_right)
+
+    if node_name[-1] == "A":
+        nodes_end_A[node_name] = (node_left, node_right)
+
+    if node_name[-1] == "Z":
+        nodes_end_Z[node_name] = (node_left, node_right)
+
+count_for_each_origin = []
+
+for node in list(nodes_end_A.keys()):
+    count = 0
+    curNode = node
+    while curNode[-1] != "Z":
+        if mode[count % len(mode)] == "L":
+            curNode = nodes[curNode][0]
+        else:
+            curNode = nodes[curNode][1]
+        count += 1
+
+    count_for_each_origin.append(count)
+
+lcm_num = count_for_each_origin[0]
+
+for num in count_for_each_origin[1:]:
+    lcm_num = lcm(lcm_num, num)
+
+print(lcm_num)
