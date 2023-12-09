9b2e9c36c5b66070b9c966fca7c8f0567c0115c2
Andy Lin <andy.lin.2004@outlook.com>
2023-12-09 00:19:56 -0500

    day 9 done

--- Day9/part1.py
+++ Day9/part1.py
@@ -1,6 +1,33 @@
-# f = open("input.txt", 'r')
-f = open("test_input.txt", 'r')
-input = f.read()
+from collections import deque
+
+f = open("input.txt", 'r')
+# f = open("test_input.txt", 'r')
+input = f.read().splitlines()
+
+total = 0
 
 for line in input:
+    layers = deque()
     
+    layers.append([int(x) for x in line.split()])
+
+    while True:
+        new_layer = []
+        for i in range(len(layers[-1]) - 1):
+            new_layer.append(layers[-1][i+1] - layers[-1][i])
+
+        layers.append(new_layer)
+
+        if set(new_layer) == {0}:
+            break
+
+    while len(layers) > 1:
+        # print(layers)
+        last_layer = layers.pop()
+        layers[-1].append(last_layer[-1] + layers[-1][-1])
+
+    # print(layers)
+    total += layers[-1][-1]      
+    # print()     
+    
+print(total)
--- Day9/part2.py
+++ Day9/part2.py
@@ -1,6 +1,35 @@
-# f = open("input.txt", 'r')
-f = open("test_input.txt", 'r')
-input = f.read()
+from collections import deque
+
+f = open("input.txt", 'r')
+# f = open("test_input.txt", 'r')
+input = f.read().splitlines()
+
+total = 0
 
 for line in input:
+    layers = deque()
     
+    reverse_arr = [int(x) for x in line.split()]
+    reverse_arr.reverse()
+    layers.append(reverse_arr)
+
+    while True:
+        new_layer = []
+        for i in range(len(layers[-1]) - 1):
+            new_layer.append(layers[-1][i+1] - layers[-1][i])
+
+        layers.append(new_layer)
+
+        if set(new_layer) == {0}:
+            break
+
+    while len(layers) > 1:
+        # print(layers)
+        last_layer = layers.pop()
+        layers[-1].append(last_layer[-1] + layers[-1][-1])
+
+    # print(layers)
+    total += layers[-1][-1]      
+    # print()     
+    
+print(total)
