877e85d689212ac48d58a6557b01ed7f0f64558d
Andy Lin <andy.lin.2004@outlook.com>
2023-12-11 00:45:36 -0500

    day 11 done

--- Day11/part1.py
+++ Day11/part1.py
@@ -1,6 +1,52 @@
-# f = open("input.txt", 'r')
+f = open("input.txt", 'r')
 f = open("test_input.txt", 'r')
-input = f.read()
+input = f.read().splitlines()
 
-for line in input:
-    
+additionalRows = set()
+additionalCols = set()
+for i in range(len(input)):
+    if "#" not in input[i]:
+        additionalRows.add(i)
+
+for i in range(len(input[0])):
+    string = "".join([input[x][i] for x in range(len(input))])
+
+    if "#" not in string:
+        additionalCols.add(i)
+
+updatedGalaxy = []
+for i in range(len(input)):
+    resultStr = ""
+    for v in range(len(input[0])):
+        resultStr += input[i][v]
+        if v in additionalCols:
+            resultStr += "."
+    updatedGalaxy.append(resultStr)
+    if i in additionalRows:
+        updatedGalaxy.append("." * (len(additionalCols) + len(input[0])))
+
+# for line in updatedGalaxy:
+#     print(line)
+
+stars = set()
+for i in range(len(updatedGalaxy)):
+    for v in range(len(updatedGalaxy[0])):
+        if updatedGalaxy[i][v] == "#":
+            stars.add((i,v))
+
+closestDistances = []
+pairsAccounted = set()
+
+for star in stars:
+    for secondStar in stars:
+        if star != secondStar and (star, secondStar) not in pairsAccounted and (secondStar, star) not in pairsAccounted:
+            distance = abs(secondStar[1] - star[1]) + abs(secondStar[0] - star[0])
+            pairsAccounted.add((star, secondStar))
+            closestDistances.append(distance)
+
+closestDistances.sort()
+
+print(stars)
+print(closestDistances)
+
+print(sum(closestDistances))
--- Day11/part2.py
+++ Day11/part2.py
@@ -1,6 +1,49 @@
-# f = open("input.txt", 'r')
-f = open("test_input.txt", 'r')
-input = f.read()
+f = open("input.txt", 'r')
+# f = open("test_input.txt", 'r')
+input = f.read().splitlines()
 
-for line in input:
-    
+additionalRows = set()
+additionalCols = set()
+for i in range(len(input)):
+    if "#" not in input[i]:
+        additionalRows.add(i)
+
+for i in range(len(input[0])):
+    string = "".join([input[x][i] for x in range(len(input))])
+
+    if "#" not in string:
+        additionalCols.add(i)
+
+stars = set()
+
+for i in range(len(input)):
+    for v in range(len(input[i])):
+        if input[i][v] == "#":
+            new_i = i
+            new_v = v
+            for row in additionalRows:
+                if row < i:
+                    new_i += 1000000 - 1
+            for col in additionalCols:
+                if col < v:
+                    new_v += 1000000 - 1
+            stars.add((new_i, new_v))
+
+            print(i, v, new_i, new_v)
+
+print(stars)
+
+closestDistances = []
+pairsAccounted = set()
+
+for star in stars:
+    for secondStar in stars:
+        if star != secondStar and (star, secondStar) not in pairsAccounted and (secondStar, star) not in pairsAccounted:
+            distance = abs(secondStar[1] - star[1]) + abs(secondStar[0] - star[0])
+            pairsAccounted.add((star, secondStar))
+            closestDistances.append(distance)
+
+closestDistances.sort()
+print(closestDistances)
+
+print(sum(closestDistances))
